a40d546192b304f0542bd0d435de7ca928fa315d
Luke Edwards <lukeedw@ad.unc.edu>
2023-08-31 14:01:50 -0400

    Completed ex00 Hello World

---
+++ lessons/evan.py
@@ -0,0 +1,8 @@
+"""Ronald"""
+
+user_name: str = input("What is your name? ")
+print("Hello, " + user_name + ", What is your favorite food?")
+food_yum: str = input(", What is your favorite food?")
+print("Youre so sexy papi chulo")
+sex_good: str = input("what are you doing tomorrow night?")
+print("I wanna get all up in them cheeks")
